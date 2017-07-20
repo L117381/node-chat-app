b02ff4015d9aadfcfb99a804a368a5492d72dacc
L117381 <Bindu.M.Vallapu@kp.org>
2017-07-20 16:42:59 -0700

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -16,6 +16,17 @@ io.on('connection', (socket) =>{
 
   console.log('New User Connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createAt: new Date().getTime()
+  });
+
+socket.broadcast.emit('newMessage', {
+  from: 'Admin',
+  text: 'New user Joined',
+  createAt: new Date().getTime()
+})
   socket.on('createMessage', (message) =>{
 
     console.log('createMessage', message);
@@ -26,10 +37,15 @@ io.on('connection', (socket) =>{
 
 
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createAt: new Date().getTime()
+    // });
   });
   socket.on('disconnect', () =>{
 
-      console.log('User was disconnected');
+    console.log('User was disconnected');
 
   })
 
